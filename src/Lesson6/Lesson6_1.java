da259c6f70a75c7f10f7250aaf469aafd232aa62
Oleh Sihunov <oleh.sihunov@openexc.com>
2022-08-22 20:23:15 +0300

    Lesson 6.

---
+++ src/Lesson6/Lesson6_1.java
@@ -0,0 +1,21 @@
+package Lesson6;
+
+import java.util.Scanner;
+
+public class Lesson6_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int numbers = -1;
+        int currentNumber;
+        do {
+            currentNumber = scanner.nextInt();
+            sum+=currentNumber;
+            numbers++;
+        } while (currentNumber != 0);
+        System.out.println("Sum: " + sum + ", Number of digits: " + numbers);
+
+
+    }
+
+}
